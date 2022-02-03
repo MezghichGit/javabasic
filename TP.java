608e4fc04f528d9b332083646ca56d91330b76f2
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-02-03 09:27:59 +0100

    les commades de base java et le tableau pramatre du main

--- TP.java
+++ TP.java
@@ -1,9 +1,13 @@
 public class TP{
 	
-	public static void main(String arg[])
+	public static void main(String m[])
 		{
 			System.out.println("Bonjour mes chers étudiants");
 			
 			System.out.println("Un super cours Java");
+			
+			System.out.println(m.length);
+			
+			System.out.println("vous avez taper :"+ m[0]);
 		}
 }
